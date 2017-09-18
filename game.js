aed1e825d2526c5624253cfd15f6ef961d2e8288
ohlsenjero <jerohlsen@gmail.com>
2017-09-19 10:45:56 +1200

    Update game.js

--- game.js
+++ game.js
@@ -26,7 +26,7 @@ var canvasBg = document.getElementById("canvasBg"),
 	ctxOverOverlay = canvasOverOverlay.getContext("2d");
 
 
-alert(canvasBg.clientWidth);
+
 
 var blockInput = false;
 
@@ -988,7 +988,7 @@ function initGame() {
 
 	},false);
 
-
+	alert(canvasBg.clientWidth);
 
 	begin();
 
